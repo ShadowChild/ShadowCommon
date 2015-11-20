e5f177a6a4754768575e95a01d99fcb3ac2ad745
ShadowChild <shadychild12@gmail.com>
2015-11-20 12:03:33 +0000

    Rewrote humanReadableByteCount

--- src/main/java/io/github/shadowchild/common/util/Utils.java
+++ src/main/java/io/github/shadowchild/common/util/Utils.java
@@ -1,16 +1,21 @@
 package io.github.shadowchild.common.util;
 
-import org.apache.commons.io.FileUtils;
+
+import org.apache.commons.math3.util.Pair;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.Observer;
 
 /**
  * Created by Zach Piddock on 07/11/2015.
  */
 public class Utils {
+
+    private static final String[] SI_UNITS = { "B", "kB", "MB", "GB", "TB", "PB", "EB" };
+    private static final String[] BINARY_UNITS = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" };
 
     /**
      * The fully customized way to the the downloader
@@ -20,7 +25,8 @@ public class Utils {
      * @param outputFolder - The folder to save the file to
      * @throws IOException
      */
-    public static void downloadFile(String url, Observer observer, File outputFolder) throws IOException {
+    public static void downloadFile(String url, Observer observer, File outputFolder) throws
+            IOException {
 
         Download download = new Download(new URL(url), observer, outputFolder);
         download.download();
@@ -55,7 +61,7 @@ public class Utils {
 
     /**
      * Gets the file name from a supplied URL
-     * 
+     *
      * @param url - The URL to get the file name from
      * @return - The String of the filename from the URL
      */
@@ -64,12 +70,31 @@ public class Utils {
         String fileName = url.getFile();
         return fileName.substring(fileName.lastIndexOf('/') + 1);
     }
-    
-    public static Object[] toHumanReadable(long size) {
-     
-        String ret = FileUtils.byteCountToDisplaySize(size);
-        String[] split = ret.split(" ");
-        
-        return split;
+
+    public static Pair<Float, String> humanReadableByteCount(long bytes, boolean useSIUnits, Locale locale) {
+
+        final String[] units = useSIUnits ? SI_UNITS : BINARY_UNITS;
+        final int base = useSIUnits ? 1000 : 1024;
+
+        // When using the smallest unit no decimal point is needed, because it's the exact number.
+        if(bytes < base) {
+            return new Pair<>((float)bytes, units[0]);
+        }
+
+        final int exponent = (int)(Math.log(bytes) / Math.log(base));
+        final String unit = units[exponent];
+        float size = Float.parseFloat(
+                String.format(locale, "%.1f", bytes / Math.pow(base, exponent)));
+        return new Pair<>(size, unit);
+    }
+
+    public static Pair<Float, String> humanReadableByteCount(long bytes, boolean useSIUnits) {
+
+        return humanReadableByteCount(bytes, useSIUnits, Locale.ENGLISH);
+    }
+
+    public static Pair<Float, String> humanReadableByteCount(long bytes) {
+
+        return humanReadableByteCount(bytes, true);
     }
 }
