5e122ccfd7a082d8b719c7f0b4b5074b1bb10e4c
ShadowChild <shadychild12@gmail.com>
2016-01-14 20:28:11 +0000

    A bit of work on the new config

--- src/main/java/io/github/shadowchild/common/newconfig/Config.java
+++ src/main/java/io/github/shadowchild/common/newconfig/Config.java
@@ -1,17 +1,45 @@
 package io.github.shadowchild.common.newconfig;
 
 
+import io.github.shadowchild.common.util.Resource;
+
 /**
  * Created by Zach Piddock on 31/12/2015.
  */
-abstract class Config {
+public abstract class Config {
 
-    public Config() {
+    final Resource location;
+    final EnumConfigType type;
+
+    public Config(Resource location, EnumConfigType type) {
+
+        this.location = location;
+        this.type = type;
+        load();
+    }
+
+    public void load() {
 
     }
 
-    public Property get(Property.EnumPropType type) {
+    public Property get(Property.EnumPropType type, String section, String propertyName, Object defaultValue) {
+
+        return get(type, section, propertyName, defaultValue, null);
+    }
+
+    public Property get(Property.EnumPropType type, String section, String propertyName, Object defaultValue, String comment) {
+
+        boolean _comment = comment != null;
+        String _section = parseSection(section);
 
         return null;
     }
+
+    /**
+     * Parses the passed section and returns the section relevant to the config type
+     *
+     * @param section
+     * @return
+     */
+    public abstract String parseSection(String section);
 }
--- src/main/java/io/github/shadowchild/common/newconfig/JsonConfig.java
+++ src/main/java/io/github/shadowchild/common/newconfig/JsonConfig.java
@@ -1,7 +1,21 @@
 package io.github.shadowchild.common.newconfig;
 
 
+import io.github.shadowchild.common.util.Resource;
+
 /**
  * Created by Zach Piddock on 03/01/2016.
  */
-public class JsonConfig extends Config {}
+public class JsonConfig extends Config {
+
+    public JsonConfig(Resource location, EnumConfigType type) {
+
+        super(location, type);
+    }
+
+    @Override
+    public String parseSection(String section) {
+
+        return null;
+    }
+}
