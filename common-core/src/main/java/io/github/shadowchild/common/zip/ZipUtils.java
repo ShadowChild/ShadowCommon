e0c10d558d07731a22638f30956cbd19d04c54f2
ShadowChild <shadychild12@gmail.com>
2016-01-25 19:59:16 +0000

    Added ZipUtils

---
+++ common-core/src/main/java/io/github/shadowchild/common/zip/ZipUtils.java
@@ -0,0 +1,75 @@
+package io.github.shadowchild.common.zip;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by Zach Piddock on 24/01/2016.
+ */
+public class ZipUtils {
+
+    /**
+     * Unzip it
+     * @param zipFile input zip file
+     * @param outputFolder zip file output folder
+     */
+    public static void unZipIt(String zipFile, String outputFolder){
+
+        byte[] buffer = new byte[1024];
+
+        try{
+
+            //create output directory is not exists
+            File folder = new File(outputFolder);
+            if(!folder.exists()){
+                folder.mkdir();
+            }
+
+            //get the zip file content
+            ZipInputStream zis =
+                    new ZipInputStream(new FileInputStream(zipFile));
+            //get the zipped file list entry
+            ZipEntry ze = zis.getNextEntry();
+
+            while(ze!=null){
+
+                String fileName = ze.getName();
+                File newFile = new File(outputFolder + File.separatorChar + fileName);
+
+                System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+
+                //create all non exists folders
+                //else you will hit FileNotFoundException for compressed folder
+                new File(newFile.getParent()).mkdirs();
+                if(ze.isDirectory()) newFile.mkdirs();
+                else newFile.createNewFile();
+
+                if(newFile.isFile()) {
+
+                    FileOutputStream fos = new FileOutputStream(newFile);
+
+                    int len;
+                    while((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+
+                    fos.close();
+                }
+                ze = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+
+            System.out.println("Done");
+
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
