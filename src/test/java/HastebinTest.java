585c183316c70e761f7f522da86c3a0fae385b50
ShadowChild <shadychild12@gmail.com>
2020-06-15 19:26:38 +0100

    Added hastebin api

---
+++ src/test/java/HastebinTest.java
@@ -0,0 +1,22 @@
+import me.shadowchild.cybernize.net.Hastebin;
+
+import java.io.IOException;
+
+public class HastebinTest {
+
+    static final String data = "This is a test\n With a newline";
+
+    public static void main(String... args) {
+
+        try {
+
+            String rawRet = Hastebin.post(data, true);
+            String ret = Hastebin.post(data, false);
+            System.out.println(rawRet);
+            System.out.println(ret);
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
